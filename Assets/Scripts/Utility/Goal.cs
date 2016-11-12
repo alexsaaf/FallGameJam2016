6d8743b88de2134c289690f46eab1cafcfdfcc85
Alexander Sääf <Alexandersaaf95@gmail.com>
2016-11-12 23:07:22 +0100

    Work on level01

---
+++ Assets/Scripts/Utility/Goal.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Goal : MonoBehaviour {
+
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D colli) {
+        if(colli.collider.tag == "Crab" || colli.collider.tag == "Duck") {
+            GameManager.instance.Won();
+        }
+
+    }
+}
