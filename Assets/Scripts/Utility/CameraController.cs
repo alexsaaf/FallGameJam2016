413da38bb4635242f3f2b356ca3a9a7a97f7769b
Alexander Sääf <Alexandersaaf95@gmail.com>
2016-11-12 20:16:37 +0100

    Add work on title screen and scene01

--- Assets/Scripts/Utility/CameraController.cs
+++ Assets/Scripts/Utility/CameraController.cs
@@ -4,10 +4,6 @@ public class CameraController : MonoBehaviour {
 
     public float moveSpeed = 10f;
 
-	void Start () {
-	
-	}
-
 	void Update () {
         Vector2 movement = transform.right * moveSpeed * Time.deltaTime;
         transform.Translate(movement);	    
