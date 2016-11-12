ba129694a7e4c62cc417bbab79d9c970976f8b0b
Alexander Sääf <Alexandersaaf95@gmail.com>
2016-11-12 16:51:46 +0100

    Add cameraMovement, water and deathtrigger

---
+++ Assets/Scripts/Utility/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float moveSpeed = 10f;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+        Vector2 movement = transform.right * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);	    
+	}
+}
---
+++ Assets/Scripts/Utility/Water.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Water : MonoBehaviour {
+
+    Camera mainCamera;
+
+	void Start () {
+        LevelInfo info = GameObject.Find("LevelInfo").GetComponent<LevelInfo>();
+        transform.position = new Vector2(transform.position.x, info.seaLevel - transform.localScale.y / 2);
+        mainCamera = Camera.main;
+    }
+
+	void Update () {
+        Vector2 newPos = new Vector2(mainCamera.transform.position.x, transform.position.y);
+        transform.position = newPos;
+	}
+}
